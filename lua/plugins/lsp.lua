fb67e3baa0a25e76fc218146001a43cc3dcdd799
DDTully <dtully99@gmail.com>
2024-06-30 11:06:37 -0500

    LSP stuff

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,50 @@
 return {
-  {
-    {
-      "williamboman/mason.nvim",
-      opts = {
-        ensure_installed = {
-          "stylua",
-          "lua-language-server",
-          "powershell-editor-services",
-          "prettier",
-          "pyright",
-        },
-      },
-    },
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-        powershell_es = {
-          filetypes = { "ps1", "psm1", "psd1" },
-          bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
-          settings = { powershell = { codeFormatting = { Preset = "OTBS", UseCorrectCasing = "true" } } },
-          init_options = {
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, {
+  --       "stylua",
+  --       "lua-language-server",
+  --       "powershell-editor-services",
+  --       "prettier",
+  --       "pyright",
+  --     })
+  --   end,
+  -- },
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "stylua",
+        "lua-language-server",
+        "powershell-editor-services",
+        "prettier",
+        "pyright",
+      })
+    end,
+  },
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      powershell_es = {
+        filetypes = { "ps1", "psm1", "psd1" },
+        bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+        settings = {
+          powershell = {
             enableProfileLoading = false,
+            codeFormatting = {
+              Preset = "OTBS",
+              UseCorrectCasing = true,
+              avoidSemicolonsAsLineTerminators = true,
+              autoCorrectAliases = true,
+              alignPropertyValuePairs = true,
+            },
           },
+        },
+        init_options = {
+          enableProfileLoading = false,
         },
       },
     },
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,14 +1,14 @@
 return {
   { "DDTully/comment-box.nvim" },
-
+  { "Mofiqul/vscode.nvim" },
   { "mbbill/undotree" },
-  {
-    "hedyhli/outline.nvim",
-    config = function()
-      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-      require("outline").setup({})
-    end,
-  },
+  -- {
+  --   "hedyhli/outline.nvim",
+  --   config = function()
+  --     vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
+  --     require("outline").setup({})
+  --   end,
+  -- },
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
